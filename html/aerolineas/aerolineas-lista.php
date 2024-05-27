d06a1a69773db229bdbc703ddc471550f4c2a41d
daviddrm52 <davidgranada2015@gmail.com>
2024-05-28 00:04:53 +0200

    detalles reseña y lista reseñas done

--- html/aerolineas/aerolineas-lista.php
+++ html/aerolineas/aerolineas-lista.php
@@ -1,9 +1,72 @@
+<?php session_start(); ?>
+
+<?php
+
+    //Hacemos la conexion
+    include('./../../config/config_bd.php');
+
+    //Query de la base de datos para recuperar los tipos de habitaci
+    $sql = "SELECT * FROM `ar-resena-aerolinea`";
+
+    //Lanzamos la query y capturamos los resultados
+    $resultados = mysqli_query($conn, $sql);
+
+    $resenas = mysqli_fetch_all($resultados, MYSQLI_ASSOC);
+
+    //Liberar memoria
+    mysqli_free_result($resultados);
+
+    //Cerramos conexión
+    mysqli_close($conn); 
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
+    <link rel="stylesheet" href="./../../css/aerolineas-lista.css">
     <title>Reseñas de aerolineas - AviaReview.com</title>
     <?php include("./../../template/header.php"); ?>
     <main class="my-3">
-        
+    <div class="container-lg text-center mt-3 rounded-3" style="background-color: #EBEBEB;">
+            <div class="row">
+                <div class="col">
+                    <p class="fs-2 mt-3">Información sobre las reseñas a aerolineas</p>
+                </div>
+            </div>
+            <div class="row align-items-center">
+                <div class="col-lg">
+                    <div class="row m-3">
+                        <div class="col">
+                            <p class="fs-5">Ultimas reseñas publicadas en AviaReview.com</p>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Identificador reseña</th>
+                                        <th scope="col">Numero de vuelo</th>
+                                        <th scope="col">Aerolinea</th>
+                                        <th scope="col">Fecha del vuelo</th>
+                                        <th scope="col">Valoración</th>
+                                        <th scope="col">Detalle de la reseña</th>
+                                    </tr>
+                                </thead>
+                                <?php foreach($resenas as $rev): ?>
+                                <tbody class="table-group-divider">
+                                    <tr>
+                                        <td><?php echo htmlspecialchars($rev['id'])?></td>
+                                        <td><?php echo htmlspecialchars($rev['codigo_vuelo_IATA'])?></td>
+                                        <td><?php echo htmlspecialchars($rev['id_aerolinea'])?></td>
+                                        <td><?php echo htmlspecialchars($rev['fecha_vuelo'])?></td>
+                                        <td><?php echo htmlspecialchars($rev['valoracion'])?></td>
+                                        <td><button class="btn boton-resena"><a href="detalles_resena.php?id=<?php echo $rev['id'] ?>">Información de la reserva</a></button></td>
+                                    </tr>
+                                </tbody>
+                                <?php endforeach; ?>
+                            </table>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
     <?php include("./../../template/footer.php"); ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- html/aerolineas/informacion-resena-aerolineas.php
+++ html/aerolineas/informacion-resena-aerolineas.php
@@ -4,7 +4,19 @@
     <title>Información de las reseñas de aerolineas - AviaReview.com</title>
     <?php include("./../../template/header.php"); ?>
     <main class="my-3">
-    <div class="container text-center mt-3 rounded-3" style="background-color: #EBEBEB;">
+        <div class="text-center" style="width: 100%;">
+            <div>
+            </div>
+        </div>
+        <div class="container text-center mt-3 rounded-3" style="background-color: #EBEBEB;">
+            <div class="row">
+                <div class="col">
+                    <p class="fs-2 mt-3">Ver las reseñas de aerolineas</p>
+                    <a href="/aviareview/html/aerolineas/aerolineas-lista.php"><button type="button" class="btn">Ver reseñas de aerolineas</button></a>
+                </div>
+            </div>
+        </div>
+        <div class="container text-center mt-3 rounded-3" style="background-color: #EBEBEB;">
             <div class="row">
                 <div class="col">
                     <p class="fs-2 mt-3">Información sobre las reseñas a aerolineas</p>
@@ -12,23 +24,36 @@
             </div>
             <div class="row align-items-center">
                 <div class="col-lg-6">
-                    <img class="figure-img img-fluid my-3 rounded-3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/HL7725%40PEK_%2820191211164812%29.jpg/1024px-HL7725%40PEK_%2820191211164812%29.jpg" alt="Boeing 737-9B5 of Korean Air (HL7725)" width="500px">
-                    <figcaption class="figure-caption">Boeing 737-9B5 de Korean Air (HL7725).</figcaption>
+                    <img class="figure-img img-fluid my-3 rounded-3" src="https://upload.wikimedia.org/wikipedia/commons/6/64/N845MH_%2826210714856%29.jpg" alt="Boeing 767-432ER of Delta Air Lines (N845MH)" width="500px">
+                    <figcaption class="figure-caption">Boeing 767-432ER de Delta Air Lines (N845MH).</figcaption>
                 </div>
                 <div class="col-lg-6 ">
                     <div class="row m-3">
                         <div class="col">
-                            <p class="fs-5">En AviaReview disponemos de reseñas de mayoria de las aerolineas que hay en servicio actualmente, en diferentes rutas y en diferentes clases.</p>
-                            <p class="fs-5">Desde aerolineas de gran calible como American Airlines (United States), hasta aerolineas domesticas como Star Rail Air (Republic of Chiquitín).</p>
+                            <p class="fs-5">En AviaReview.com se pueden hacer reseñas de las aerolineas en las rutas que realizan, un ejemplo es un vuelo de Vueling desde Barcelona a Menorca.</p>
+                            <p class="fs-5">Todas las reseñas de vuelos mantienen esta estructura:</p>
+                            <ul class="list-group">
+                                <li class="list-group-item">Nombre de la aerolinea</li>
+                                <li class="list-group-item">Aeropuerto de origen</li>
+                                <li class="list-group-item">Aeropuerto de destino</li>
+                                <li class="list-group-item">Usuario que ha realizado la reseña</li>
+                                <li class="list-group-item">Codigo del vuelo</li>
+                                <li class="list-group-item">Fecha del vuelo</li>
+                                <li class="list-group-item">Asiento del vuelo</li>
+                                <li class="list-group-item">Descripción breve</li>
+                                <li class="list-group-item">Experiencia en embarque</li>
+                                <li class="list-group-item">Modelo de aeronave</li>
+                                <li class="list-group-item">Interior del avión</li>
+                                <li class="list-group-item">Experiencia en el despegue</li>
+                                <li class="list-group-item">Servicios a bordo (Catering)</li>
+                                <li class="list-group-item">Experiencia lavabos</li>
+                                <li class="list-group-item">Experiencia aterrizaje</li>
+                                <li class="list-group-item">Experiencia desembarque</li>
+                                <li class="list-group-item">Experiencia en caso de incidencia</li>
+                                <li class="list-group-item">Valoración, puntos positivos y negativos</li>
+                            </ul>
+                            <p class="fs-5">No todos los puntos son obligatorios al realizar una reseña, con esto se consigue mantener una estructura homogenea en todas las reseñas, asi los usuarios pueden comparar como es el servicio de catering de la aerolinea por ejemplo con multiples aerolineas.</p>
                        </div>
-                    </div>
-                    <div class="row m-3">
-                        <div class="col-7">
-
-                        </div>
-                        <div class="col-5">
-                            <a href="./html/aerolineas/aerolineas-lista.php"><button type="button" class="btn btn-lg">Ver reseñas de aerolineas</button></a>
-                        </div>
                     </div>
                 </div>
             </div>
